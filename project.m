0d60db2a49acc5ba7b408bf9dca585ca0d98fef9
Derek429 <57273208+Derek429@users.noreply.github.com>
2019-11-01 16:43:10 -0500

    Update project.m

--- project.m
+++ project.m
@@ -5,6 +5,7 @@ close all;
 materialList = {'Concrete' 16 30 96000 900 5 5;
              'Wood' 23 53 115000 800 12 11;
              'Adobe' 18 42 68000 600 6 5};
+%Hello
 
 choice = menu('Wall Material', materialList{:,1});
 
